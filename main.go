6626c81c951bce3bfbd33f93a549404dc9576474
Lucas Lara <lucaslorentzlara@hotmail.com>
2020-07-18 02:04:24 +0200

    Change plugins

--- main.go
+++ main.go
@@ -8,16 +8,13 @@ import (
 	// Plugins
 	_ "github.com/lucaslorentz/caddy-docker-proxy/plugin"
 
-	// DNS Providers
-	_ "github.com/caddyserver/dnsproviders/azure"
-	_ "github.com/caddyserver/dnsproviders/cloudflare"
-	_ "github.com/caddyserver/dnsproviders/digitalocean"
-	_ "github.com/caddyserver/dnsproviders/godaddy"
-	_ "github.com/caddyserver/dnsproviders/googlecloud"
-	_ "github.com/caddyserver/dnsproviders/route53"
-
 	// Plugins
+	_ "github.com/BTBurke/caddy-jwt"
+	_ "github.com/caddyserver/dnsproviders/cloudflare"
+	_ "github.com/captncraig/caddy-realip"
+	_ "github.com/jung-kurt/caddy-cgi"
 	_ "github.com/nicolasazrak/caddy-cache"
+	_ "github.com/tarent/loginsrv/caddy"
 
 	// Caddy
 	"github.com/caddyserver/caddy/caddy/caddymain"
